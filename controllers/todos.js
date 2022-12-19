0097c5cbe1b20984ac19884789306571bbd1dd33
Mohamed Abdulwahab <mabdulwahab.dev@gmail.com>
2022-12-18 21:00:37 -0800

    added server-side requests and database commands to mark-complete, mark-incomplete, and delete to-do items from the to-do lists on different user accounts

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,3 +1,4 @@
+const { findOneAndUpdate } = require('../models/Todo');
 const Todo = require('../models/Todo');
 
 module.exports = {
@@ -22,4 +23,32 @@ module.exports = {
             console.log(err);
         }
     },
+    deleteTodo: async (req, res) => {
+        try {
+            const deleteItem = await Todo.deleteOne({ _id: req.body.todoIdFromJSFile });
+            console.log('Deleted item from database');
+            res.json('Deleted item from database');
+        } catch (error) {
+            console.log(error);
+        }
+    }, 
+    markComplete: async (req, res) => {
+        try {
+            const markComplete = await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile }, { completed: true });
+            console.log('item marked completed on database');
+            res.json('item marked completed');
+        } catch(error) {
+            console.log(error);
+        }
+    }, 
+    markIncomplete: async (req, res) => {
+        console.log(req.body);
+        try {
+            const markIncomplete = await Todo.findOneAndUpdate({ _id: req.body.todoIdFromJSFile}, { completed: false });
+            console.log('item marked incompleted on database');
+            res.json('item marked incompleted on database');
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
